d2c56f94ce173bd445934af3fc55cd874d79ca82
luis-zaldivar <lalozaldivarbarza@gmail.com>
2023-05-14 21:24:04 -0600

    se eliminaron lineas que no se ocubana

--- video 3.0 copy.py
+++ video 3.0 copy.py
@@ -12,16 +12,11 @@ import whisper
 import pandas as pd
 import os
 from whisper.utils import WriteSRT
-
-
 # crear la instancia de VLC
 instance = vlc.Instance('--no-xlib')
 player = instance.media_player_new()
 player.set_fullscreen(True)
-
 # Función para extraer un frame del video
-
-
 def extract_frame():
     # Pausa el reproductor de video
     player.pause()
@@ -42,7 +37,6 @@ def extract_frame():
     img = cv2.circle(frame, (377,253), 63, (0, 0, 255), -1)
     # Pide al usuario que seleccione un archivo para guardar el fotograma
     file_path = filedialog.asksaveasfilename(defaultextension=".jpg")
-
     # escribir fotograma modificado en el video
     out = cv2.VideoWriter(player.get_media().get_mrl(), cv2.VideoWriter_fourcc(*'mp4v'), cap.get(
         cv2.CAP_PROP_FPS), (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))))
@@ -54,7 +48,6 @@ def extract_frame():
         if cap.get(cv2.CAP_PROP_POS_FRAMES) == frame_index:
             continue  # saltar fotograma a reemplazar
         out.write(frame)
-
     if file_path:
         # Guarda el fotograma en el archivo seleccionado
         cv2.imwrite(file_path, img)
@@ -68,47 +61,18 @@ def extract_frame():
         img_label.pack()
     else:
         print("No se seleccionó ningún archivo para guardar el fotograma.")
-
-
-
-        
-
 # Función para abrir un archivo de video
 def open_file():
     file_path = filedialog.askopenfilename(filetypes=[("Video Files", "*.mp4 *.avi *.mkv *.mp3")])
     media = instance.media_new(file_path)
     player.set_media(media)
-    '''
-    # Obtener la ruta relativa del archivo
-    ruta_relativa = os.path.join(os.getcwd(), file_path)
-    print(ruta_relativa)
-    # Ruta del archivo de salida MP3
-    output_file = "archivo.mp3"
-
-    # Comando de ffmpeg para convertir el archivo MP4 a MP3
-    command = f"ffmpeg -i {ruta_relativa} -vn -acodec libmp3lame -qscale:a 2 {output_file}"
-
-    # Ejecutar el comando de ffmpeg
-    subprocess.call(command, shell=True)
-    
-    model = whisper.load_model("medium")
-    result = model.transcribe("archivo.mp3")
-    srt_path = os.path.join("", "Sub.srt")
-    write_str = WriteSRT(srt_path)
-    with open(srt_path, "w", encoding="utf-8") as srt_file:
-        write_str.write_result(result, srt_file, options={'max_line_width': 250, 'max_line_count': 250, 'highlight_words': True})
-    '''
     player.play()
-
 # Función para pausar el video
 def pause():
     player.pause()
-
 # Función para detener el video
 def stop():
     player.stop()
-
-
 def update():
     currentTime = int(player.get_time() / 1000)  # read current time
     totalTime = int(player.get_length() / 1000)  # read total time
@@ -118,8 +82,6 @@ def update():
     durationLabel.config(text='{0:02d}:{1:02d}'.format(minutes, seconds))
     progressBar.config(value=currentTime, maximum=totalTime)  # configure the play bar
     root.after(1000, update)
-
-
 # reads the cursor event in the play bar
 def progressBarClic(event):
     x = event.x  # read event
@@ -127,14 +89,10 @@ def progressBarClic(event):
     position = x / progressBar.winfo_width()
     timeJump = int(totalTime * position)  # jumps at the right time
     player.set_time(timeJump * 1000)  # reproduce at the right time
-
-
 def borrar_archivo():
     archivo = "archivo.mp3"
     if os.path.exists(archivo):
         os.remove(archivo)
-
-
 def cerrar_ventana():
     borrar_archivo()
     root.destroy()
@@ -146,24 +104,19 @@ def MosSub():
 # crear la root principal de tkinterdis
 root = tk.Tk()
 root.title("Reproductor de Video")
-
 # agregar la barra de progreso
 load = False
 playing = True
-
 currentTimeLabel = ttk.Label(root, text='00:00')
 currentTimeLabel.grid(row=1,column=0)
 durationLabel = ttk.Label(root, text='00:00')
 durationLabel.grid(row=1, column=6)
-
 s = ttk.Style()
 s.theme_use('clam')
 s.configure("red.Horizontal.TProgressbar", foreground='red', background='red')
 progressBar = ttk.Progressbar(root, style = 'red.Horizontal.TProgressbar', orient='horizontal', length=600, mode='determinate')
 progressBar.grid(row=1, column=1)
 progressBar.bind('<Button-1>', progressBarClic)
-
-
 BTopen=ttk.Button(root,text="abir",command=open_file)
 BTopen.place(x=0,y=475)
 ToolTip(BTopen,msg="selecciona un archivo de video para reproducir")
@@ -172,16 +125,12 @@ Pausas.place(x=0,y=520)
 ToolTip(Pausas,msg="Pausar el video en reproduccion")
 parada=ttk.Button(root,text="parar",command=stop)
 parada.place(x=100,y=475)
-#Sub=ttk.Button(root,text="cc",command=MosSub)
-#Sub.place(x=590,y=475)
 ToolTip(parada,msg="detiene la reproduccion del video ")
 imaframe=ttk.Button(root,text="extraer frame",command=extract_frame)
 imaframe.place(x=100,y=520)
 ToolTip(imaframe,msg="extra el frame que se esta viendo en imagen ")
-#ToolTip(Sub, msg="Mostar Subtitulos  ")
 load = False
 playing = True
-
 currentTimeLabel = ttk.Label(root, text='00:00')
 currentTimeLabel.grid(row=1,column=0)
 durationLabel = ttk.Label(root, text='00:00')
